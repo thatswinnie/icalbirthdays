078f54f860139c66d463a1385b540a3cc82cd0c8
wteichmann <wteichmann@e1c0543e-163d-0410-8a19-69080c1d5ac0>
2010-01-31 21:17:37 +0000

    initial commit of iCalBirthdays in Cocoa

---
+++ trunk/iCalBirthdays_Cocoa/BirthdayEvent.h
@@ -0,0 +1,17 @@
+//
+//  BirthdayEvent.h
+//  cocoaTest
+//
+//  Created by thatswinnie on 1/28/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CalendarStore/CalendarStore.h>
+
+
+@interface BirthdayEvent : CalEvent {
+	
+}
+
+@end
---
+++ trunk/iCalBirthdays_Cocoa/BirthdayPerson.h
@@ -0,0 +1,33 @@
+//
+//  BirthdayPerson.h
+//  cocoaTest
+//
+//  Created by thatswinnie on 1/28/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AddressBook/AddressBook.h>
+
+
+@interface BirthdayPerson : ABPerson {
+	NSString *eventTitle;
+	NSString *reminderTitle;
+	NSString *fullName;
+	NSInteger age;
+}
+
+@property (nonatomic, retain) NSString *eventTitle;
+@property (nonatomic, retain) NSString *reminderTitle;
+@property (nonatomic, retain) NSString *fullName;
+@property NSInteger age;
+
+
+- (id) initWith: (ABPerson *) person;
+- (NSString *) constructEventTitle: (NSInteger) formatIndex customTitleText: (NSString *) customTemplate;
+- (NSString *) constructReminderTitle: (NSInteger) formatIndex customTitleText: (NSString *) customTemplate;
+- (NSString *) constructAlertTitle: (NSInteger) formatIndex customTitleText: (NSString *) customTemplate;
+- (NSString *) constructFullName;
+- (NSInteger) calculateAge;
+
+@end
